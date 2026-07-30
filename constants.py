56a1f2d66b4d5adf857108a53c2222a830032669
hellfire <jimmyrairigh@gmail.com>
2026-07-30 13:11:59 -0500

    Now have ship that rotates

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+#the radius of the players ship
+PLAYER_RADIUS = 20
+#the width of the lines that draw the players ship
+LINE_WIDTH = 2
+PLAYER_TURN_SPEED = 300
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     running = True
@@ -15,9 +17,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
-        clock.tick(60)
+        player.draw(screen)
         dt = clock.tick(60) / 1000
+        player.update(dt)
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
