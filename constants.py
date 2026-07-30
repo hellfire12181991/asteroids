a05c8858109a1981655f78efe4c4e712f1ec013e
hellfire <jimmyrairigh@gmail.com>
2026-07-30 13:35:20 -0500

    Working drawable and updatable groups for player, also moves

--- constants.py
+++ constants.py
@@ -5,3 +5,4 @@ PLAYER_RADIUS = 20
 #the width of the lines that draw the players ship
 LINE_WIDTH = 2
 PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
--- main.py
+++ main.py
@@ -7,19 +7,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     running = True
+
     while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        for update in updatable:
+            update.update(dt)
         pygame.display.flip()
 
 
