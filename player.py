f4788f600d71a838b070742074a49aa1d229eb62
hellfire <jimmyrairigh@gmail.com>
2026-07-30 16:57:42 -0500

    fully optimized and ready for tomorrow

--- player.py
+++ player.py
@@ -4,7 +4,6 @@ from circleshape import CircleShape
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-
         self.rotation = 0
 
     # in the Player class
