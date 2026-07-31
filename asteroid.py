ee3fb63051573bdc1d966947c99ae0070d1a69d7
hellfire <jimmyrairigh@gmail.com>
2026-07-31 12:00:48 -0500

    Finished Asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
         super().__init__(x, y, radius)
@@ -10,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position = self.position + dt * self.velocity
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        first_velocity = self.velocity.rotate(angle)
+        second_velocity = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first_child = Asteroid(self.position.x, self.position.y, new_radius)
+        first_child.velocity = first_velocity * 1.2
+        second_child = Asteroid(self.position.x, self.position.y, new_radius)
+        second_child.velocity = second_velocity * 1.2
--- main.py
+++ main.py
@@ -36,6 +36,12 @@ def main():
                 log_event("player_hit")
                 print ("Game over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
         screen.fill("black")
         for draw in drawable:
             draw.draw(screen)
