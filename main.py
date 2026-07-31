b04d4b834afc9ccbbbda54731f882da7f7c92056
hellfire <jimmyrairigh@gmail.com>
2026-07-31 11:35:00 -0500

    Added asteroids and the ship shoots with a cooldown

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
-from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+from logger import log_state, log_event
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -9,18 +13,29 @@ def main():
     dt = 0.0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable, )
+    Shot.containers = (shots, updatable, drawable)
+    asteroidfield = AsteroidField()
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     while True:
         log_state()
+        for update in updatable:
+            update.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        for update in updatable:
-            update.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print ("Game over!")
+                sys.exit()
         screen.fill("black")
         for draw in drawable:
             draw.draw(screen)
